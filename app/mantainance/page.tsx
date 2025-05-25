665c1108e0cd38a6afb31f73a98ab1471586a766
nischal-lc <lamichhanen96@gmail.com>
2025-05-25 07:56:08 +0545

    random

--- app/mantainance/page.tsx
+++ app/mantainance/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="h-screen w-screen font-bricolage">
+    <div className="h-screen w-screen font-bricolage flex items-center justify-center">
       <h1 className="text-3xl ">
         This website is currently under maintainance
       </h1>
